df319f2aef48b45d47ac203db5a1dd8c67984668
Spy230 <121127523+Spy230@users.noreply.github.com>
2024-08-30 17:45:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-// src/App.js
 import React from 'react';
-import './App.css';
-import Form from './components/Form/Form';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Form from './components/Form/Form';;  
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Contact Form</h1>
-          <Form />
-        </header>
-      </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Form />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+
+
+
